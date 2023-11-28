98dc524ebeaf233e85c67f3514371abeb46c54f0
stman3 <alhodalysalman@gmail.com>
2023-11-28 18:39:54 +0300

    clean the code.

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import ListUsers, CartView
+from .views import CartView
 
 urlpatterns = [
-    path("list-users/", ListUsers.as_view(), name="list-users"),
     path("cart/menu-items/", CartView.as_view(), name="CartView"),
 ]
